da83713364b5972b6846e24dc6068c1f8ce60dd0
pokan975 <44811052+pokan975@users.noreply.github.com>
2018-12-30 23:27:06 -0700

    Add files via upload

--- study_code.py
+++ study_code.py
@@ -245,19 +245,31 @@
 # print(x+z+sum(y))
 # =============================================================================
 
-S = "loveleetcode"
-C = 'e'
-
-def go(m):
-    if C in m:
-        return m.index(C)
-    else:
-        return len(S)+1
-
-ans = []
-for idx in range(len(S)):
-    lm, rm = S[:idx], S[idx:]
-    ans.append(min(go(lm),go(rm)))
+# =============================================================================
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+# 
+# class Solution:
+#     def mergeTrees(self, t1, t2):
+#         """
+#         :type t1: TreeNode
+#         :type t2: TreeNode
+#         :rtype: TreeNode
+#         """
+#         if t1 == None:
+#             return t2
+#         if t2 == None:
+#             return t1
+#         
+#         t1.val = t1.val+t2.val
+#         t1.left = self.mergeTrees(t1.left,t2.left)
+#         t1.right = self.mergeTrees(t1.right,t2.right)
+#         
+#         return t1
+# =============================================================================
     
 
 
