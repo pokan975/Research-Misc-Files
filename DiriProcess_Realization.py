c480aa469e6985ce3d9def4c0c5e7a9031e04bf6
pokan975 <44811052+pokan975@users.noreply.github.com>
2019-10-24 16:36:05 -0700

    add implementation of chinese restaurant process

--- DiriProcess_Realization.py
+++ DiriProcess_Realization.py
@@ -2,8 +2,6 @@
 """
 Created on Mon Oct 21 15:26:49 2019
 @author: WilliamShih
-This program simulates generating samples from Dirichlet process using
-stick-breaking approach, takes standard Gaussian as base distribution
 """
 
 import numpy as np
@@ -12,7 +10,12 @@ import matplotlib.pyplot as plt
 from functools import reduce
 
 np.random.seed(1)
-
+# concentration parameter
+alpha = 2
+# =============================================================================
+# This part of code simulates the realization of Dirichlet process using
+# stick-breaking approach, taking stanard Gaussian as base distribution
+# =============================================================================
 def Stick_Breaking(num_weights, alpha):
     betas = np.random.beta(1, alpha, size = num_weights) 
     betas[1:] *= np.cumprod(1 - betas[:-1])
@@ -20,15 +23,12 @@ def Stick_Breaking(num_weights, alpha):
 
 # number of samples
 n = 10
-# alpha for Dirichlet distribution
-alpha = 1
-
 sample = np.zeros(n)
 
 # generate realization from G_0 (base distribution)
 x = np.random.normal(0., 1., n)
 # generate theta from beta distribution
-theta = np.random.beta(alpha, 1, n)
+theta = np.random.beta(1, alpha, n)
 # compute Dirichlet samples
 sample[0] = theta[0]
 
@@ -40,18 +40,57 @@ for i in range(1, n):
 # referred from stackoverflow
 weights = Stick_Breaking(n, alpha)
 
+plt.rcParams['figure.dpi'] = 100
 plt.rcParams['font.size'] = 12
 # plot base distribution
 xx = np.linspace(-3, 3, 100)
 yy = np.fromiter(map(lambda x: st.norm(0., 1.).pdf(x), xx), dtype = np.float)
 
-plt.stem(x, sample, label = "a realization")
+plt.stem(x, sample)
 plt.plot(xx, yy, 'g-', label = "base dist.")
-plt.xlabel("$G_0$ (Gaussian)")
-plt.ylabel("Weight")
+plt.xlabel(r"$\theta_i$ (from standard normal)")
+plt.ylabel(r"Prob. Weight ($\pi_i$)")
 plt.legend()
 plt.grid()
 plt.show()
 # check if samples sum to 1
-print("Sum of all weights:")
+print("Sum of total prob. weights:")
 print(sum(sample), sum(weights))
+
+
+# =============================================================================
+# This part of code simulates the realization of Dirichlet process using
+# Chinese restaurant process (CRP)
+# =============================================================================
+# initialize table array
+tables = [1, alpha]
+# record the growth of table number
+tablenum = [1]
+# size of dataset
+customer = 5000
+
+for c in range(1, customer):
+    # set prob. of new customer selecting each table
+    table_prob = np.array(tables) / sum(tables)
+    # table selection as a realization of categorical distribution
+    assignment = np.random.multinomial(1, table_prob)
+    # show which table customer selects
+    t = np.argwhere(assignment)
+    
+    # customer selects new table, add a table with 1 customer
+    if t[0][0] == len(tables) - 1:
+        tables.insert(-2, 1)
+        tablenum.append(tablenum[-1] + 1)
+    # customer selects one of current tables, add 1 to that table
+    else:
+        tables[t[0][0]] += 1
+        tablenum.append(tablenum[-1])
+        
+print("\nChinese restaurant process \nTable population")
+for i, t in enumerate(tables[:-1]):
+    print("{0:5} {1:10}".format(i+1, tables[i]))
+plt.plot(tablenum)
+plt.xlabel("customer index")
+plt.ylabel("table number")
+plt.grid()
+plt.show()
