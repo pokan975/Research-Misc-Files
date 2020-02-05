8b5cf504bd3ac544e5427905149f6eedd9dfc3e6
pokan975 <44811052+pokan975@users.noreply.github.com>
2020-02-04 19:37:02 -0700

    bayes inference visualized

--- Bayes_inference.py
+++ Bayes_inference.py
@@ -8,59 +8,104 @@ import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
 
-
-class NormalInvWishart(object):
-    def __init__(self, mu, lmbda, nu, psi):
-        self.mu = mu
-        self.lmbda = float(lmbda)
-        self.nu = nu
-        self.psi = psi
-        self.inv_psi = np.linalg.inv(psi)
-
-    def sample(self):
-        sigma = np.linalg.inv(self.wishartrand())
-        return (np.random.multivariate_normal(self.mu, sigma / self.lmbda), sigma)
-
-    def wishartrand(self):
-        dim = self.inv_psi.shape[0]
-        chol = np.linalg.cholesky(self.inv_psi)
-        foo = np.zeros((dim, dim))
-
-        for i in range(dim):
-            for j in range(i + 1):
-                if i == j:
-                    foo[i,j] = np.sqrt(st.chi2.rvs(self.nu - (i + 1) + 1))
-                else:
-                    foo[i,j]  = np.random.normal(0, 1)
-        return np.dot(chol, np.dot(foo, np.dot(foo.T, chol.T)))
+plt.rcParams['figure.dpi'] = 100
+plt.rcParams['font.size'] = 12
 
 
-# generate samples
-mean = 5
-var = 3
+# =============================================================================
+# Likelihood Function:
+# compute the joint likelihood of the samples given every mean and variance sampled
+# from prior distribution. Since samples are assumed drawn i.i.d,
+# joint likelihood can be factored into product of likelihood of each sample
+# take normal distribution as likelihood distribution
+# =============================================================================
+def Likelihood(data, mu, var):
+    # initialize the joint likelihood value
+    like = 1
+    
+    for datum in data:
+        # calc likelihood of each sample, then multiply them
+        pdf_val = st.norm(mu, np.sqrt(var)).pdf(datum)
+        like *= pdf_val
+    
+    # return likelihood value of the sample set
+    return like
 
-sample = st.norm(mean, var).rvs(200)
 
-# hyperparameters of prior normal
-mu = 0
-sigma = np.var(sample)
+# =============================================================================
+# generate samples (dataset) from a normal distribution
+# =============================================================================
+# parameters of true distribution that samples are from
+mean = 5  # true mean
+var = 3   # true variance
+# sample from true distribution
+sample = st.norm(mean, var).rvs(20)
 
-# hyperparameter of prior inverse gamma
+
+# =============================================================================
+# mean & variance joint prior distribution
+# divide the joint prior into 2 indep. priors for mean & variance, respectively
+# for variance, use its reciprocal, precision, for prior dist. instead
+# so Normal inverse Gamma (joint prior) => Normal (mean) * inverse Gamma (precision)
+# =============================================================================
+# define value ranges of mean & precision priors
+mean_prior = np.linspace(0, 10, 50)
+prec_prior = np.linspace(0.1, 1, 50)
+
+
+# hyperparameters of mean prior (normal distribution)
+hyper_mu = 3
+hyper_sigma = np.var(sample)
+
+# hyperparameter of precision prior (inverse gamma dist.) (beta = 1)
 alpha = 0.1
 
-m = np.linspace(-5, 5, 100)
-t = np.linspace(0, 10, 100)
 
-x, y = np.meshgrid(m, t)
+# calc mean & precision priors probability values, respectively
+mean_prior_pdf = st.norm(hyper_mu, np.sqrt(hyper_sigma)).pdf(mean_prior)
+prec_prior_pdf = st.invgamma(alpha).pdf(prec_prior)
 
-prior_m = st.norm(mu, np.sqrt(sigma)).pdf(m)
-prior_t = st.invgamma(alpha).pdf(t)
-prior = np.zeros((len(t), len(m)))
+prior = np.zeros((len(prec_prior), len(mean_prior)))
 
-for i in range(t.shape[0]):
-    prior[i, :] = prior_t[i] * prior_m[:]
+# combine the joint prior as product of mean prior & precision prior
+for i in range(prec_prior.shape[0]):
+    prior[i, :] = prec_prior_pdf[i] * mean_prior_pdf[:]
 
+
+# plot the 2D colormap of the joint prior
 prior = np.flipud(prior)
 # imshow: extent = [x_min , x_max, y_min , y_max] sets x, y axis values
-plt.imshow(prior, cmap = "plasma", extent=[-5, 5, 0, 10])
+plt.imshow(prior, cmap = "plasma", extent = [0, 10, 0.1, 1], aspect = "auto")
+plt.xlabel("mean value")
+plt.ylabel("precision value")
+plt.title("prior (Normal Inverse Gamma) distribution\ncolor represents prior probability value")
+plt.show()
+
+
+# =============================================================================
+# mean & variance joint posterior distribution
+# for every (mean, precision) pair in joint prior, calc its corresponding
+# proportional posterior = likelihood * prior
+# doesn't take prob. of sample set into account, it's constant for mean & precision
+# =============================================================================
+posterior = np.zeros((len(prec_prior), len(mean_prior)))
+
+# for every (mean, precision) point in joint prior, calc posterior
+for i in range(prec_prior.shape[0]):
+    for j in range(mean_prior.shape[0]):
+        # variance = 1/(precision)
+        vari = prec_prior[i]**(-1)
+        # calc likelihood given (mean, variance) parameter pair
+        L = Likelihood(sample, mean_prior[j], vari)
+        # calc proportional posterior
+        posterior[i, j] = prec_prior_pdf[i] * mean_prior_pdf[j] * L
+        
+
+# plot the 2D colormap of the joint posterior
+posterior = np.flipud(posterior)
+# imshow: extent = [x_min , x_max, y_min , y_max] sets x, y axis values
+plt.imshow(posterior, cmap = "plasma", extent = [0, 10, 0.1, 1], aspect = "auto")
+plt.xlabel("mean value")
+plt.ylabel("precision value")
+plt.title("posterior distribution\ncolor represents prior probability value")
 plt.show()
