241fbd1d9625e1d6a98b2b71d2f734b969cb3987
pokan975 <44811052+pokan975@users.noreply.github.com>
2020-07-27 10:16:48 -0700

    Add files via upload

--- Gibbs_DPMM.py
+++ Gibbs_DPMM.py
@@ -9,11 +9,13 @@ for simplification, the variance of components are constant & the same
 base dist. is only over mean of all components
 """
 
+import sys
 import numpy as np
 import scipy.stats as st
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
+sys.getdefaultencoding()
 plt.rcParams['figure.dpi'] = 100
 plt.rcParams['font.size'] = 12
 np.random.seed(1)
@@ -31,13 +33,13 @@ alpha = 2  # concentration parameter of DP prior
 # tunable parameters
 N = 200  # number of samples
 components = 3  # number of mixture components
-iterations = 10**2  # times of Gibbs sampling iteration
+iterations = 10**4  # times of Gibbs sampling iteration
 
 
 # =============================================================================
 # Draw samples from finite GMM
 # =============================================================================
-def sampleMM(n, K):
+def sampleMM(n, K, print_out = False):
     '''
     Parameters
     ----------
@@ -59,6 +61,8 @@ def sampleMM(n, K):
     # use Gaussian dist. to generate the mean for each component,
     # take round just for simplification
     comp_mean = np.round(np.random.normal(g0_mean, g0_var, K), 2)
+    if print_out == True:
+        print("component mean: ", comp_mean)
 
     # pick component for each sample
     pick_component = np.random.multinomial(1, comp_prob, size = n)
@@ -102,8 +106,8 @@ class Gibbs_sampler(object):
         self.cluster_num = [1]
         # record the members of each cluster, each cluster represented as a list
         self.clusters = [list(self.data)]
-        # record the size of each cluster (last one always represents newly created one)
-        self.cluster_size = [self.size, alpha]
+        # record the history of partitions over samples
+        self.partition_history = []
         # record the mean of each cluster
         self.cluster_mu = [np.random.normal(g0_mean, np.sqrt(g0_var))]
         # initialize all cluster assignment indicators as zero 
@@ -152,6 +156,8 @@ class Gibbs_sampler(object):
 
 
     def Gibbs_iteration(self):
+        # initialize the size of each cluster (last one always represents newly created one)
+        cluster_size = [self.size, alpha]
         comp_std = np.sqrt(comp_var)
         # Gibbs sampling iteration
         for t in range(self.iters):
@@ -165,15 +171,15 @@ class Gibbs_sampler(object):
                 # remove sample[i] from current assignment
                 # current assignment for sample[i] is c[i]
                 self.clusters[self.c[i]].remove(self.data[i])
-                self.cluster_size[self.c[i]] -= 1
-                assert self.cluster_size[self.c[i]] >= 0
+                cluster_size[self.c[i]] -= 1
+                # assert cluster_size[self.c[i]] >= 0
                 
                 # sample[i] was singleton of its orig. cluster and now it left, cluster num - 1
-                if self.cluster_size[self.c[i]] == 0:
+                if cluster_size[self.c[i]] == 0:
                     self.cluster_num[-1] -= 1
         
                 # build probability vector for cluster assignment
-                prior_prob = np.array(self.cluster_size) / (self.size - 1 + alpha)
+                prior_prob = np.array(cluster_size) / (self.size - 1 + alpha)
                 
                 # build the posterior prob. of being assigned to each existing cluster
                 likelihood = st.norm(self.cluster_mu, comp_std).pdf(self.data[i])
@@ -194,9 +200,9 @@ class Gibbs_sampler(object):
                 self.c[i] = c_i_new
         
                 # sample[i] assigned to new cluster
-                if c_i_new == len(self.cluster_size) - 1:
+                if c_i_new == len(cluster_size) - 1:
                     # add a cluster with 1 member and assign sample[i] to it
-                    self.cluster_size.insert(-1, 1)
+                    cluster_size.insert(-1, 1)
                     self.clusters.append([self.data[i]])
                     # draw parameter for newly created cluster
                     self.cluster_mu.append(self.G0_posterior([self.data[i]]))
@@ -205,18 +211,18 @@ class Gibbs_sampler(object):
         
                 # sample[i] assigned to one of current clusters
                 else:
-                    self.cluster_size[c_i_new] += 1
+                    cluster_size[c_i_new] += 1
                     self.clusters[c_i_new].append(self.data[i])
             
             # reassign indices in indicator set c by removing empty clusters 
             temp = self.c
-            for i, val in enumerate(self.cluster_size):
+            for i, val in enumerate(cluster_size):
                 if val == 0:
                     self.c = np.where(temp > i, self.c - 1, self.c)
                 
             # remove empty clusters from sets clusters, cluster_size
-            while 0 in self.cluster_size:
-                self.cluster_size.remove(0)
+            while 0 in cluster_size:
+                cluster_size.remove(0)
                 self.clusters.remove([])
                 # self.cluster_mu.pop(i)  # comment out
                 
@@ -225,6 +231,11 @@ class Gibbs_sampler(object):
             # =============================================================================
             # for non-empty cluster, update its parameter using its members
             self.cluster_mu[:] = list(map(self.G0_posterior, self.clusters))
+            
+            # record the partition of samples at each iteration
+            # (pad each partition with empty clusters to a large enough number of clusters)
+            pad_length = 30 - len(cluster_size)
+            self.partition_history.append(np.pad(cluster_size, (0, pad_length), "constant", constant_values = 0))
 
 
 
@@ -232,16 +243,21 @@ class Gibbs_sampler(object):
 # Main function starts here
 # =============================================================================
 # generate data for fixed sample size
-samples = sampleMM(N, components)
+samples = sampleMM(N, components, True)
 # do Gibbs sampling for this data set
 G = Gibbs_sampler(N, iterations, samples)
 G.Gibbs_iteration()
 
+# print experimental parameters
+print("components of GMM: ", components)
+print("data points: ", N)
+print("\u03B1: ", alpha)
+
 # plot histogram of data
 plt.hist(samples, bins = 50, density = True)
 plt.xlabel("value")
-plt.ylabel("probability density")
-plt.title("histogram of samples")
+plt.ylabel("normalized histogram")
+plt.title("sample distribution")
 plt.show()
 
 # plot num of clusters vs. iterations
@@ -249,27 +265,41 @@ ii = np.arange(0, iterations + 1)
 plt.plot(ii, G.cluster_num, "-o")
 plt.xlabel("iteration")
 plt.ylabel("cluster number")
-plt.title("number of clusters vs. iterations")
+plt.grid()
+plt.show()
+
+# plot the clusters vs. avg number of samples at each cluster
+p = np.array(G.partition_history)
+# take avg over all partition histories for each cluster
+p_avg = np.mean(p, axis = 0)
+ii = np.arange(1, 30 + 1, 1)
+plt.bar(ii, p_avg)
+plt.xlabel("cluster index")
+plt.ylabel("avg. number of samples")
 plt.grid()
 plt.show()
 
 
-# test different sample sizes
-sample_size = np.arange(100, 260, 10)
-# expected number of clusters of Gibbs sampling result for each sample size
-avg_cluster = []
-
-for nn in sample_size:
-    samples = sampleMM(nn, components)
-    G = Gibbs_sampler(nn, iterations, samples)
-    G.Gibbs_iteration()
-    # compute expected number of clusters for each sampling result
-    avg_cluster.append(np.mean(G.cluster_num))
-    
-# plot number of clusters vs. number of samples
-plt.plot(sample_size, avg_cluster)
-plt.xlabel("sample size")
-plt.ylabel("cluster number")
-plt.title("number of clusters vs. sample size")
-plt.grid()
-plt.show()
+# =============================================================================
+# # test different sample sizes
+# sample_size = np.arange(150, 260, 10)
+# iterations = 8000
+# # expected number of clusters of Gibbs sampling result for each sample size
+# avg_cluster = []
+# 
+# for nn in sample_size:
+#     print("size: ", nn)
+#     samples = sampleMM(nn, components)
+#     G = Gibbs_sampler(nn, iterations, samples)
+#     G.Gibbs_iteration()
+#     # compute expected number of clusters for each sampling result
+#     avg_cluster.append(np.mean(G.cluster_num))
+#     
+# # plot number of clusters vs. number of samples
+# plt.plot(sample_size, avg_cluster)
+# plt.xlabel("sample size")
+# plt.ylabel("cluster number")
+# plt.title("number of clusters vs. sample size")
+# plt.grid()
+# plt.show()
+# =============================================================================
